2beabb10caaaa169a373846583cc68291771f318
Abdul-Fattah99 <qabdulfatai@gmail.com>
2021-01-18 11:06:10 +0100

    setup retrofit Got an api key from newsapi.org, created jsontokotlin class using the converter pugin

---
+++ app/src/main/java/com/example/newsapp/ui/api/NewsAPI.kt
@@ -0,0 +1,33 @@
+package com.example.newsapp.ui.api
+
+import com.example.newsapp.ui.NewsResponse
+import com.example.newsapp.ui.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode:String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey:String = API_KEY
+
+    ): Response<NewsResponse>
+
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("country")
+        searchQuery:String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey:String = API_KEY
+
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "c248dc08d2e84a07b7ca7e46501ebe3d"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
